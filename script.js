5657e0bd89ab7085d32054d0a06ed440dc923dff
Evgeniy Savin <82032696+Dexader@users.noreply.github.com>
2022-11-22 22:49:17 +0700

    Add Comments

--- script.js
+++ script.js
@@ -6,17 +6,3 @@ menuBtn.addEventListener('click', function () {
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 });
-
-let desktopMenu = document.querySelector('.menu__body');
-
-function transferringContent(x) {
-	if(x.matches) {
-		menu.innerHTML = desktopMenu.innerHTML + menu.innerHTML;
-	} else {}
-}
-
-const mediaQuery = window.matchMedia('(max-width: 767.98px)');
-
-transferringContent(mediaQuery);
-
-mediaQuery.addEventListener('matches', transferringContent);
