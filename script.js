6bf3d5826bfb8b744c33340e2caf606a9e7032d4
Evgeniy Savin <82032696+Dexader@users.noreply.github.com>
2022-11-29 00:17:07 +0700

    Pricing page finish

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 
 let menuBtn = document.querySelector('.burger__button');
 let menu = document.querySelector('.burger__body');
+let body = document.querySelector('.body');
 menuBtn.addEventListener('click', function () {
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
+	body.classList.toggle('active');
 });
